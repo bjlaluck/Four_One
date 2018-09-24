586509bb2c9e802e10ffbcaf4bbf3152114e84e2
Bohdan Laluck <bohdanlaluck@hotmail.com>
2018-09-23 21:12:23 -0400

    changes for 5.1

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,11 @@
 Rails.application.routes.draw do
-  get 'orders/index'
-  get 'orders/show'
+
   get 'simple_pages/landing_page'
   resources :products
   get 'simple_pages/about'
   get 'simple_pages/contact'
   get 'simple_pages/index'
-  root 'simple_pages#landing_page'
+  root 'products#index'
   resources :orders, only: [:index, :show, :create, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
